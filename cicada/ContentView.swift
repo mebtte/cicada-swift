5e31cc1edaa2fccc15fd76754c75c053ed42eadd
mebtte <hi@mebtte.com>
2024-03-18 19:42:51 +0800

    init

---
+++ cicada/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  cicada
+//
+//  Created by MEBTTE P on 2024/3/18.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ cicada/cicadaApp.swift
@@ -0,0 +1,17 @@
+//
+//  cicadaApp.swift
+//  cicada
+//
+//  Created by MEBTTE P on 2024/3/18.
+//
+
+import SwiftUI
+
+@main
+struct cicadaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
